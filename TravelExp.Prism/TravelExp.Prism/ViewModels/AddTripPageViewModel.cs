5b9980d509451d729e94903b0d11828409c467d3
ALEX JHOJAN BOLIVAR VARGAS <alexbolivar240835@correo.itm.edu.co>
2020-04-15 19:08:50 -0500

    Add Syncfusion Calendar to Add trip

--- TravelExp.Prism/TravelExp.Prism/ViewModels/AddTripPageViewModel.cs
+++ TravelExp.Prism/TravelExp.Prism/ViewModels/AddTripPageViewModel.cs
@@ -9,9 +9,18 @@ namespace TravelExp.Prism.ViewModels
 {
     public class AddTripPageViewModel : ViewModelBase
     {
+
+        private bool _isRunning;
         public AddTripPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             Title = "Add Trip";
+            IsRunning = false;
+        }
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set => SetProperty(ref _isRunning, value);
         }
     }
 }
